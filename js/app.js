370b62468eb19e572f73b1159d5e8e8c90243406
AraceliGS <aragutarras@gmail.com>
2018-03-27 15:37:43 -0500

    añadiendo avances- iterando svg

---
+++ js/app.js
@@ -0,0 +1,31 @@
+// debugger;
+// var section = document.getElementById('svg-section');
+// for (i = 0; i <= (7-1); i++) {
+//   var svg = document.createElement('svg');
+//   svg.id="svg-" + i;
+//   svg.setAttribute('viewBox', "0 0 400 200");
+//   var ellipse = document.getElementsByTagName('ellipse')[0];
+//   svg.appendChild(ellipse);
+//   console.log(svg);
+//   section.appendChild(svg);
+//   console.log(section);
+// }
+
+// console.log(document.getElementById('svg'))
+
+
+// var svg = document.createElement('svg');
+// svg.id="svg-" + i
+// svg.setAttribute('viewBox', '0 ' + i + ' 400 200')
+// // console.log(svg)
+// var ellipse = document.createElement('ellipse');
+// ellipse.setAttribute('cx', 200);
+// ellipse.setAttribute('cy', 100);
+// ellipse.setAttribute('rx', 95);
+// ellipse.setAttribute('ry', 70);
+// // console.log(ellipse);
+// svg.appendChild(ellipse);
+// document.body.appendChild(svg);
+// console.log(svg)
+// // section.appendChild(svg);
+// // console.log(section);
